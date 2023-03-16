c74a76d60e46fb223dd1c5174c4b0bb8a1f9903c
Rolando Garcia <rogarcia@berkeley.edu>
2023-03-16 18:46:42 +0000

    flor instrumentation

--- main.py
+++ main.py
@@ -1,79 +1,92 @@
 import torch
 import torch.nn as nn
+from torch.utils import data as torchdata
 import torchvision
 import torchvision.transforms as transforms
 
+import flor
+from flor import MTK as Flor
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = flor.log(
+    "device", torch.device("cuda" if torch.cuda.is_available() else "cpu")
+)
 
-# Hyper-parameters 
+# Hyper-parameters
 input_size = 784
-hidden_size = 500
+hidden_size = flor.log("hidden_size", 500)
 num_classes = 10
 num_epochs = 5
-batch_size = 100
-learning_rate = 0.001
+batch_size = flor.log("batch_size", 100)
+learning_rate = flor.log("lr", 0.001)
 
-# MNIST dataset 
-train_dataset = torchvision.datasets.MNIST(root='../data', 
-                                           train=True, 
-                                           transform=transforms.ToTensor(),  
-                                           download=True)
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(
+    root="../data", train=True, transform=transforms.ToTensor(), download=True
+)
 
-test_dataset = torchvision.datasets.MNIST(root='../data', 
-                                          train=False, 
-                                          transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST(
+    root="../data", train=False, transform=transforms.ToTensor()
+)
 
 # Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
-                                           shuffle=True)
+train_loader = torchdata.DataLoader(
+    dataset=train_dataset, batch_size=batch_size, shuffle=True
+)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
-                                          shuffle=False)
+test_loader = torchdata.DataLoader(
+    dataset=test_dataset, batch_size=batch_size, shuffle=False
+)
 
 # Fully connected neural network with one hidden layer
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
-    
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
         return out
 
+
 model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 total_step = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
+Flor.checkpoints(model, optimizer)
+for epoch in Flor.loop(range(num_epochs)):
+    for i, (images, labels) in Flor.loop(enumerate(train_loader)):
         # Move tensors to the configured device
-        images = images.reshape(-1, 28*28).to(device)
+        images = images.reshape(-1, 28 * 28).to(device)
         labels = labels.to(device)
-        
+
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+        if (i + 1) % 100 == 0:
+            print(
+                "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                    epoch + 1,
+                    num_epochs,
+                    i + 1,
+                    total_step,
+                    flor.log("loss", loss.item()),
+                )
+            )
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
@@ -81,11 +94,15 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
-        images = images.reshape(-1, 28*28).to(device)
+        images = images.reshape(-1, 28 * 28).to(device)
         labels = labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+    print(
+        "Accuracy of the network on the 10000 test images: {} %".format(
+            100 * correct / total
+        )
+    )
